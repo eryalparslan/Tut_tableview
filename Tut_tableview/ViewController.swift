d83fe1ce58476bb49daad86364c46e146286ff93
Eray Alparslan <erayalparslan@yahoo.com>
2018-09-19 12:04:00 +0300

    iOS Notları

--- Tut_tableview/ViewController.swift
+++ Tut_tableview/ViewController.swift
@@ -9,17 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mTableView: UITableView!
+    let myArray = ["Pirzola", "Biftek", "Gerdan", "Antrikot", "Bonfile", "Pençata", "Döş", "Nuar", "Tranç"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mTableView.delegate = self
+        mTableView.dataSource = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myArray.count
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as UITableViewCell
+        let myTitle =  myArray[indexPath.row]
+        cell.textLabel?.text = myTitle
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
 }
 
